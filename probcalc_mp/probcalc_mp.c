5fdfc24868ae39b925f540c7c58ab4327898da1a
Adrian Dalca <shrimp@cs.toronto.edu>
2009-02-06 19:08:04 +0000

    doubleized ints in add_p_stats
    git-svn-id: svn+ssh://combine-101/home/svn_bio/trunk@310 450597fd-3c47-0410-84d8-f85f2794411d

--- probcalc_mp/probcalc_mp.c
+++ probcalc_mp/probcalc_mp.c
@@ -852,8 +852,7 @@ inline int add_p_stats(mapping_t * fwd_map, mapping_t * rev_map,
 		pgenome = pgenome_fwd * pgenome_rev;
 	} else {
 		pgenome_bin = floor((ABS(double(dist) - gl_mean) 
-				* 1.0 / hist_distcutoff) 
-				* HIST_BINS);
+				* 1.0 / double(hist_distcutoff)) * double(HIST_BINS));
 	
 		if (pgenome_bin >= HIST_BINS)
 			pgenome_cumsum = 0;
