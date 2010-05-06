2c0f709d5de29fc5ffd854dea8b9cfd1698cb83e
Daniel Lister <shrimp@cs.toronto.edu>
2010-05-06 22:43:13 +0000

    it works now
    git-svn-id: svn+ssh://combine-101/home/svn_bio/shrimp/branches/dev@597 450597fd-3c47-0410-84d8-f85f2794411d

--- utils/findpairs.py
+++ utils/findpairs.py
@@ -4,8 +4,8 @@ if len(sys.argv) < 6:
 	print >> sys.stderr, 'usage %s <suffix 1> <suffix 2> <paired output file> <unpaired output file> <readfile1> <readfile2> [...]' % (sys.argv[0])
 	sys.exit(1)
 
-suffix1 = sys.argv[1]
-suffix2 = sys.argv[2]
+suffix1 = sys.argv[1] + '\n'
+suffix2 = sys.argv[2] + '\n'
 
 paired_output_file = open(sys.argv[3],'w')
 unpaired_output_file = open(sys.argv[4],'w')
@@ -31,21 +31,20 @@ def compare(x,y):
 	return cmp(x[0],y[0])
 
 reads.sort(cmp=compare)
-
 i = 0
 
 while i < len(reads):
 	if reads[i][0].endswith(suffix1):
-		if i+1<len(reads) and reads[i+1][0].endswith(suffix2):
-			pout('\n'.join(reads[i]))
-			pout('\n'.join(reads[i + 1]))
+		if i+1<len(reads) and reads[i+1][0].endswith(suffix2) and reads[i][0][:-1*len(suffix1)] == reads[i + 1][0][:-1*len(suffix2)]:
+			pout(''.join(reads[i]))
+			pout(''.join(reads[i + 1]))
 			i += 2
 			continue
 	elif reads[i][0].endswith(suffix2):
-		if i+1<len(reads) and reads[i+1][0].endswith(suffix1):
-			pout('\n'.join(reads[i]))
-			pout('\n'.join(reads[i + 1]))
+		if i+1<len(reads) and reads[i+1][0].endswith(suffix1) and reads[i][0][:-1*len(suffix2)] == reads[i + 1][0][:-1*len(suffix1)]:
+			pout(''.join(reads[i + 1]))
+			pout(''.join(reads[i]))
 			i += 2
 			continue
-	upout('\n'.join(reads[i]))
+	upout(''.join(reads[i]))
 	i += 1
