a0e79d40b9d68547149c22d84d9f6e5b8b22d2e1
Michael Dzamba <michael.dzamba@utoronto.ca>
2011-02-08 09:12:09 +0000

    OSX DOES NOT HAVE MEMRCHRmakemake ARGGHHH
    git-svn-id: svn+ssh://combine-101/home/svn_bio/shrimp/trunk@712 450597fd-3c47-0410-84d8-f85f2794411d

--- mergesam/file_buffer.c
+++ mergesam/file_buffer.c
@@ -8,6 +8,18 @@
 
 #define MIN(a,b) (((a) < (b)) ? (a) : (b))
 
+void * memchr(const void *s, int c, size_t n)
+{
+	if (n != 0) {
+		const unsigned char *p = (const unsigned char *)s;
+
+		do {
+			if (*p++ == (unsigned char)c)
+				return ((void *)(p - 1));
+		} while (--n != 0);
+	}
+	return (NULL);
+}
 
 void fb_close(file_buffer * fb) {
 	free(fb->base);
