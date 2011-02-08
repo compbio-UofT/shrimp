27e104bb78048bafc0bc4b7320fcc9fdd291e440
Michael Dzamba <michael.dzamba@utoronto.ca>
2011-02-08 09:35:42 +0000

    2.1.1 up on site
    git-svn-id: svn+ssh://combine-101/home/svn_bio/shrimp/trunk@713 450597fd-3c47-0410-84d8-f85f2794411d

--- mergesam/file_buffer.c
+++ mergesam/file_buffer.c
@@ -7,18 +7,16 @@
 #include "file_buffer.h"
 
 #define MIN(a,b) (((a) < (b)) ? (a) : (b))
-
-void * memchr(const void *s, int c, size_t n)
-{
-	if (n != 0) {
-		const unsigned char *p = (const unsigned char *)s;
-
-		do {
-			if (*p++ == (unsigned char)c)
-				return ((void *)(p - 1));
-		} while (--n != 0);
-	}
-	return (NULL);
+void * memrchr(const void *s, int c, size_t n) {
+    const unsigned char *cp;
+    if (n != 0) {
+	cp = (unsigned char *)s + n;
+	do {
+	    if (*(--cp) == (unsigned char)c)
+		return((void *)cp);
+	} while (--n != 0);
+    }
+    return((void *)0);
 }
 
 void fb_close(file_buffer * fb) {
