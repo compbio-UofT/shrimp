508fa26f357756fe346eead6274a5b143a16db8c
Matei David <matei@cs.toronto.edu>
2011-04-11 19:15:39 +0000

    seed hits in sam output
    git-svn-id: svn+ssh://combine-101/home/svn_bio/shrimp/branches/region-filter@779 450597fd-3c47-0410-84d8-f85f2794411d

--- gmapper/output.c
+++ gmapper/output.c
@@ -600,8 +600,8 @@ hit_output(struct read_entry * re, struct read_hit * rh, struct read_hit * rh_mp
 	//extra = extra + sprintf(extra,"\tAS:i:%d\tH0:i:%d\tH1:i:%d\tH2:i:%d\tNM:i:%d\tNH:i:%d\tIH:i:%d",rh->sfrp->score,hits[0],hits[1],hits[2],rh->sfrp->mismatches+rh->sfrp->deletions+rh->sfrp->insertions,found_alignments,stored_alignments);
 		//MERGESAM DEPENDS ON SCORE BEING FIRST!
 	*output_buffer += snprintf(*output_buffer,output_buffer_end-*output_buffer,
-		"\tAS:i:%d\tH0:i:%d\tH1:i:%d\tH2:i:%d\tNM:i:%d\tNH:i:%d\tIH:i:%d",
-		rh->sfrp->score,hits[0],hits[1],hits[2],rh->sfrp->mismatches+rh->sfrp->deletions+rh->sfrp->insertions,satisfying_alignments,stored_alignments);
+		"\tAS:i:%d\tH0:i:%d\tH1:i:%d\tH2:i:%d\tNM:i:%d\tNH:i:%d\tIH:i:%d\tX0:i:%d",
+				   rh->sfrp->score,hits[0],hits[1],hits[2],rh->sfrp->mismatches+rh->sfrp->deletions+rh->sfrp->insertions,satisfying_alignments,stored_alignments, rh->matches);
 	if (shrimp_mode == COLOUR_SPACE){
 		//TODO
 		//int first_bp = re->initbp[0];
