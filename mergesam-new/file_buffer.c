f9ce9ec12983589b84ed27a76d4897753bbb4366
Matei David <matei@cs.toronto.edu>
2011-07-27 17:11:38 +0000

    mergesam: old version that used to be in branches/region-filter mergesam-new: newer version that used to be in trunk; DOES NOT COMPILE
    git-svn-id: svn+ssh://combine-101/home/svn_bio/shrimp/trunk@845 450597fd-3c47-0410-84d8-f85f2794411d

--- mergesam-new/file_buffer.c
+++ mergesam-new/file_buffer.c
@@ -6,18 +6,18 @@
 #include <assert.h>
 #include "file_buffer.h"
 
-#define MIN(a,b) (((a) < (b)) ? (a) : (b))
-void * memrchr(const void *s, int c, size_t n) {
+//#define MIN(a,b) (((a) < (b)) ? (a) : (b))
+/*void * memrchr(const void *s, int c, size_t n) {
     const unsigned char *cp;
     if (n != 0) {
 	cp = (unsigned char *)s + n;
 	do {
 	    if (*(--cp) == (unsigned char)c)
 		return((void *)cp);
-	} while (--n != 0);
+ 	} while (--n != 0);
     }
     return((void *)0);
-}
+}*/
 
 void fb_close(file_buffer * fb) {
 	free(fb->base);
@@ -43,6 +43,7 @@ file_buffer * fb_open(char * path,size_t buffer_size,size_t read_size) {
 	fb->size=buffer_size;
 	fb->unseen_start=0;
 	fb->unseen_end=0;
+	fb->unseen_inter=0;
 	//allocate the read buffer
 	fb->frb.base=(char*)malloc(read_size);
 	if (fb->frb.base==NULL) {
@@ -96,28 +97,22 @@ void fill_read_buffer(file_read_buffer * frb) {
 	//fprintf(stderr,"fill_read_buffer : %lu seen, %lu unseen, %lu filled, %lu size, EOF %d\n",frb->seen,frb->unseen,frb->filled, frb->size, frb->eof);
 }
 
-
+//find the last new line in the area and 
 void mark_partial_line(file_read_buffer * frb,size_t offset) {
 	assert(frb->seen+frb->unseen==frb->filled);
-	//fprintf(stderr,"mark_partial_line : %lu seen, %lu unseen, %lu filled, %d eof\n",frb->seen,frb->unseen,frb->filled, frb->eof);
-	//char buffer[129];
-	//memcpy(buffer,frb->base,frb->filled);
-	//buffer[frb->filled]='\0';
-	//fprintf(stderr,"|%s|\n",buffer);
+	//find out if the offset is bigger then what has not been seen yet
+	// if it is use this instead of the offset
+	//for example if we have 20 unseen characters and user is requesting offset of 40 characters, only look at the last 20
 	frb->exhausted=offset>=frb->unseen;
-	//if (frb->eof==1) {
-	//	return;
-	//}
 	size_t unseen=frb->exhausted ? frb->unseen : offset;
-	//fprintf(stderr,"mark_partial_line : unseen %lu, %lu offset, %lu unseen\n",unseen,offset,frb->unseen);
+	//find the last newline, set the unseen count accordingly
 	char * ptr = (char*)memrchr(frb->base+frb->seen,'\n',unseen);
-	if (ptr==NULL) {
-		//fprintf(stderr,"mark_partial_line : HIT NULL\n");
-		//frb->unseen=frb->filled;
-	} else {
+	if (ptr!=NULL) {
 		size_t offset=(ptr-frb->base)+1;
 		frb->unseen=frb->filled-offset;
-		//fprintf(stderr,"mark_partial_line : HIT NOT NULL\n");
+	} else if (frb->eof==1) {	
+		fprintf(stderr,"EOF\n");
+		frb->unseen=0;
 	}
 }
 
@@ -125,109 +120,64 @@ void mark_partial_line(file_read_buffer * frb,size_t offset) {
 //unseen_start is always 0 based actual location of start
 //unseen_end is always 0 based location of next to be written data
 void add_read_buffer_to_main(file_buffer * fb) {
-	//fprintf(stderr,"ADDING\n");
 	size_t unseen_end_mod=fb->unseen_end%fb->size;
 	size_t unseen_start_mod=fb->unseen_start%fb->size;
 	fb->changed=false;
 	assert(fb->frb.seen+fb->frb.unseen==fb->frb.filled);
 	assert(fb->unseen_start<=fb->unseen_end);
+	if (fb->unseen_start+fb->size<fb->unseen_end) {
+		fprintf(stderr,"%lu+%lu<%lu\n",fb->unseen_start,fb->size,fb->unseen_end);
+	}
 	assert(fb->unseen_start+fb->size>=fb->unseen_end);
+	//while the buffer is not filled to the top and we have stuff left to read in the frb
 	while (fb->unseen_end!=fb->unseen_start+fb->size && !fb->frb.exhausted) {
-		//fprintf(stderr,"%lu unseen_end_mod, %lu unseen_startmod, %s exhausted\n",unseen_end_mod,unseen_start_mod,fb->frb.exhausted ? "YES" : "NO");
-		//fprintf(stderr,"%lu unseen_end, %lu unseen_start, %s exhausted\n",fb->unseen_end,fb->unseen_start,fb->frb.exhausted ? "YES" : "NO");
 		assert(fb->unseen_start+fb->size>=fb->unseen_end);
 		assert(fb->frb.seen+fb->frb.unseen==fb->frb.filled);
 		assert(fb->unseen_start<=fb->unseen_end);
-		//fprintf(stderr,"%lu - %lu , %lu - %lu\n",unseen_start_mod,unseen_end_mod,fb->size,unseen_end_mod);
+		//find out how much continous space we have to the end of the buffer or start
 		size_t space=(unseen_start_mod>unseen_end_mod ? unseen_start_mod : fb->size ) - unseen_end_mod;
-		//fprintf(stderr,"space %lu\n",space);
 		assert(space>0);
+		//find out how far to look in buffer for last newline
 		mark_partial_line(&fb->frb,space);
 		size_t just_seen = fb->frb.filled-fb->frb.seen-fb->frb.unseen;
-		//fprintf(stderr,"add_read_buffer_to_main : %lu just_seen, %lu frb.filled, %lu frb.seen, %lu frb.unseen\n",just_seen,fb->frb.filled,fb->frb.seen,fb->frb.unseen);
+		//unseen was just updated in mark_partial_line if some new lines were found
+		//just_seen being non-zero means that we found some new lines to move over, so do it
 		if (just_seen!=0) {
-			//fprintf(stderr,"COPY IN\n");
-			//means we can copy some in
 			memmove(fb->base+unseen_end_mod,fb->frb.base+fb->frb.seen,just_seen);
 			fb->changed=true;
 			fb->frb.seen+=just_seen;
 			assert(fb->frb.seen+fb->frb.unseen==fb->frb.filled);
 			fb->unseen_end+=just_seen;
 			unseen_end_mod=fb->unseen_end%fb->size;
+		//in this case we did not find any new lines to move over and there is more
+		//data waiting on the frb, so fb needs to go into exhausted state
 		} else if (!fb->frb.exhausted) {
-			//fprintf(stderr,"add_read : 3\n");
 			fb->exhausted=true;
 		}
-		//fprintf(stderr,"%lu unseen_end_mod, %lu unseen_startmod, %s exhausted\n",unseen_end_mod,unseen_start_mod,fb->frb.exhausted ? "YES" : "NO");
-		//fprintf(stderr,"%lu unseen_end, %lu unseen_start, %s exhausted\n",fb->unseen_end,fb->unseen_start,fb->frb.exhausted ? "YES" : "NO");
 		if (!fb->frb.exhausted) {
+			//if the frb is not exhausted that means fb space was limiting, so fill in the rest with 0's
+			//check if start_mod < end_mod or of the case if buffer has 0 bytes unseen
 			if (unseen_start_mod<unseen_end_mod || fb->unseen_start==fb->unseen_end) { 
-				//fprintf(stderr,"PADDING\n");
 				assert(fb->size>=unseen_end_mod);
+				//fill to end with zeros, since can't fit any more in, because !fb->frb.exhausted
 				memset(fb->base+unseen_end_mod,'\0',fb->size-unseen_end_mod);
 				fb->changed=true;
 				fb->unseen_end+=fb->size-unseen_end_mod;
 				unseen_end_mod=fb->unseen_end%fb->size;
 			} else {
-				//fprintf(stderr,"add_read : 4\n");
 				fb->exhausted=true;	
+				assert(fb->unseen_start+fb->size>=fb->unseen_end);
 				return;
 			}
 		}
 		unseen_end_mod=fb->unseen_end%fb->size;
 	}
 	if (fb->unseen_end==fb->unseen_start+fb->size || (fb->frb.exhausted && fb->frb.eof==1)) {
-		//fprintf(stderr,"add_read : 5\n");
 		fb->exhausted=true;
 	}
 	assert(fb->frb.seen+fb->frb.unseen==fb->frb.filled);
 	assert(fb->unseen_start<=fb->unseen_end);
-		assert(fb->unseen_start+fb->size>=fb->unseen_end);
-	//fprintf(stderr,"condition %lu + %lu != %lu\n",fb->unseen_start,fb->size,fb->unseen_end);
+	assert(fb->unseen_start+fb->size>=fb->unseen_end);
 }
-	/*if (unseen_end_mod>unseen_start_mod) {
-		if (unseen_end_mod==0) {
-			assert(fb->frb.seen+fb->frb.unseen==fb->frb.filled);
-			if (fb->frb.pad) {
-				//fill top
-				size_t space_top=unseen_start_mod;
-				if (space_top>0) {
-					mark_partial_line(&fb->frb,space_top);
-					size_t just_seen = fb->frb.filled-fb->frb.seen-fb->frb.unseen;
-					if (just_seen!=0) {
-						//means we can copy some in
-						memmove(fb->base,fb->frb.base+fb->frb.seen,just_seen);
-						fb->frb.seen+=just_seen;
-						assert(fb->frb.seen+fb->frb.unseen==fb->frb.filled);
-						fb->unseen_end+=just_seen;
-						unseen_end_mod=fb->unseen_end%fb->size;
-					}
-				}
-			}
-		}
 
-	} else {
-		//fill only middle
-		size_t space_middle = (fb->unseen_end==fb->unseen_start) ? fb->size : unseen_start_mod-unseen_end_mod;
-		if (space_middle!=0) {
-			mark_partial_line(&fb->frb,space_middle);	
-			size_t just_seen = fb->frb.filled-fb->frb.seen-fb->frb.unseen;
-			if (just_seen!=0) {
-				//means we can copy some in
-				memmove(fb->base+unseen_end_mod,fb->frb.base+fb->frb.seen,just_seen);
-				fb->frb.seen+=just_seen;
-				assert(fb->frb.seen+fb->frb.unseen==fb->frb.filled);
-				fb->unseen_end+=just_seen;
-				unseen_end_mod+=just_seen;
-				assert(unseen_end_mod<fb->size);
-			}
-			if (fb->frb.pad) { 
-				size_t pad_size = 
-				assert(unseen_start_mod>=unseen_end_mod);
-				fprintf(stderr,"padding starting at %lu , size of %lu + %lu = %lu\n",unseen_end_mod,unseen_start_mod,unseen_end_mod, unseen_start_mod-unseen_end_mod);
-				memset(fb->base+unseen_end_mod,'\n',unseen_start_mod-unseen_end_mod);
-				fb->unseen_end+=unseen_start_mod-unseen_end_mod;
-			}
-		}
-	}*/	
 
